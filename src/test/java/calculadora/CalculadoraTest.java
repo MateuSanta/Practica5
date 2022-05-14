ef71b1fadeed28d2863f37c20643ffa6350d71d0
Carlosherreram <cherrera@cifpfbmoll.eu>
2022-05-14 23:14:36 +0200

    añadidos tests

--- src/test/java/calculadora/CalculadoraTest.java
+++ src/test/java/calculadora/CalculadoraTest.java
@@ -19,30 +19,30 @@ Calculadora calculadora = new Calculadora();
     @org.junit.Test
     public void testSumar() {
         
-        double resultado = calculadora.sumar(4.0, 2.0);
-        double esperado = 6.0; //  4+ + 2 = 6
+        double resultado = calculadora.sumar(5.3, 2.8);
+        double esperado = 8.1; //  5.3+ + 2.8 = 8.1
         assertEquals(esperado,resultado,0);
     }
 
     @org.junit.Test
     public void testRestar() {
-        double resultado = calculadora.restar(4.0, 2.0);
-        double esperado = 2.0; // 4-2 = 2
+        double resultado = calculadora.restar(5.3, 2.8);
+        double esperado = 2.5; // 5.3-2.8 = 2.5
         assertEquals(esperado,resultado,0);
     }
 
     @org.junit.Test
     public void testMultiplicar() {
-        double resultado = calculadora.multiplicar(4.0, 2.0);
-        double esperado = 8.0; // 4*2 = 8
+        double resultado = calculadora.multiplicar(5.3, 2.8);
+        double esperado = 14.84; // 5.3*2.8 = 14.84
         assertEquals(esperado,resultado,0);
     }
 
     @org.junit.Test
     public void testDividir() {
-        double resultado = calculadora.restar(4.0, 2.0);
-        double esperado = 2.0; // 4/2 = 2 
-        assertEquals(esperado,resultado,0);
+        double resultado = calculadora.restar(5.3, 2.8);
+        double esperado = 1.89; // 4.3/2.8 = 1.89
+        assertEquals(esperado,resultado,0.01);
     }
     
 }
